2f3495b143e4ec4793db055a1d81795a47d96b83
mark-pro <20671988+mark-pro@users.noreply.github.com>
2026-02-18 19:01:50 -0500

    refactor: move ThenEnsure methods to file bottoms

--- src/ErrorOr/ErrorOr.Then.cs
+++ src/ErrorOr/ErrorOr.Then.cs
@@ -36,24 +36,6 @@ public readonly partial record struct ErrorOr<TValue> : IErrorOr<TValue>
     }
 
     /// <summary>
-    /// If the state is a value, the provided function <paramref name="onValue"/> is executed and its errors are returned.
-    /// If no errors are returned, the original value is returned.
-    /// </summary>
-    /// <param name="onValue">The function to execute if the state is a value.</param>
-    /// <returns>The errors from calling <paramref name="onValue"/> if state is value and errors are returned; otherwise the original <see cref="ErrorOr"/> instance.</returns>
-    public ErrorOr<TValue> ThenEnsure(Func<TValue, ErrorOr<TValue>> onValue)
-    {
-        if (IsError)
-        {
-            return Errors;
-        }
-
-        ErrorOr<TValue> result = onValue(Value);
-
-        return result.IsError ? result.Errors : this;
-    }
-
-    /// <summary>
     /// If the state is a value, the provided function <paramref name="onValue"/> is executed and its result is returned.
     /// </summary>
     /// <typeparam name="TNextValue">The type of the result.</typeparam>
@@ -103,6 +85,40 @@ public readonly partial record struct ErrorOr<TValue> : IErrorOr<TValue>
     }
 
     /// <summary>
+    /// If the state is a value, the provided function <paramref name="onValue"/> is executed asynchronously and its result is returned.
+    /// </summary>
+    /// <typeparam name="TNextValue">The type of the result.</typeparam>
+    /// <param name="onValue">The function to execute if the state is a value.</param>
+    /// <returns>The result from calling <paramref name="onValue"/> if state is value; otherwise the original <see cref="Errors"/>.</returns>
+    public async Task<ErrorOr<TNextValue>> ThenAsync<TNextValue>(Func<TValue, Task<TNextValue>> onValue)
+    {
+        if (IsError)
+        {
+            return Errors;
+        }
+
+        return await onValue(Value).ConfigureAwait(false);
+    }
+
+    /// <summary>
+    /// If the state is a value, the provided function <paramref name="onValue"/> is executed and its errors are returned.
+    /// If no errors are returned, the original value is returned.
+    /// </summary>
+    /// <param name="onValue">The function to execute if the state is a value.</param>
+    /// <returns>The errors from calling <paramref name="onValue"/> if state is value and errors are returned; otherwise the original <see cref="ErrorOr"/> instance.</returns>
+    public ErrorOr<TValue> ThenEnsure(Func<TValue, ErrorOr<TValue>> onValue)
+    {
+        if (IsError)
+        {
+            return Errors;
+        }
+
+        ErrorOr<TValue> result = onValue(Value);
+
+        return result.IsError ? result.Errors : this;
+    }
+
+    /// <summary>
     /// If the state is a value, the provided function <paramref name="onValue"/> is executed asynchronously and its errors are returned.
     /// If no errors are returned, the original value is returned.
     /// </summary>
@@ -118,21 +134,5 @@ public readonly partial record struct ErrorOr<TValue> : IErrorOr<TValue>
         ErrorOr<TValue> result = await onValue(Value).ConfigureAwait(false);
 
         return result.IsError ? result.Errors : this;
-    }
-
-    /// <summary>
-    /// If the state is a value, the provided function <paramref name="onValue"/> is executed asynchronously and its result is returned.
-    /// </summary>
-    /// <typeparam name="TNextValue">The type of the result.</typeparam>
-    /// <param name="onValue">The function to execute if the state is a value.</param>
-    /// <returns>The result from calling <paramref name="onValue"/> if state is value; otherwise the original <see cref="Errors"/>.</returns>
-    public async Task<ErrorOr<TNextValue>> ThenAsync<TNextValue>(Func<TValue, Task<TNextValue>> onValue)
-    {
-        if (IsError)
-        {
-            return Errors;
-        }
-
-        return await onValue(Value).ConfigureAwait(false);
     }
 }
