dee3effbaa309133dc9b47fb35fbed3c266af6cf
mark-pro <20671988+mark-pro@users.noreply.github.com>
2026-04-30 22:05:10 -0400

    test: add ThenEnsure chaining coverage

--- tests/ErrorOr/ErrorOr.ThenEnsureAsyncTests.cs
+++ tests/ErrorOr/ErrorOr.ThenEnsureAsyncTests.cs
@@ -58,6 +58,63 @@ public class ThenEnsureAsyncTests
     }
 
     [Fact]
+    public async Task CallingThenEnsureAsyncAfterThenAsync_WhenEnsureSucceeds_ShouldContinueChainWithOriginalValue()
+    {
+        // Arrange
+        ErrorOr<string> errorOrString = "5";
+        int valueFromThenDo = 0;
+
+        // Act
+        ErrorOr<string> result = await errorOrString
+            .ThenAsync(Convert.ToIntAsync)
+            .Then(num => num * 2)
+            .ThenEnsureAsync(num => Task.FromResult<ErrorOr<int>>(num > 3 ? num + 100 : Error.Validation()))
+            .ThenDoAsync(num =>
+            {
+                valueFromThenDo = num;
+                return Task.CompletedTask;
+            })
+            .Then(Convert.ToString);
+
+        // Assert
+        result.IsError.Should().BeFalse();
+        result.Value.Should().Be("10");
+        valueFromThenDo.Should().Be(10);
+    }
+
+    [Fact]
+    public async Task CallingThenEnsureAsyncAfterThenAsync_WhenEnsureReturnsError_ShouldNotInvokeLaterChain()
+    {
+        // Arrange
+        ErrorOr<string> errorOrString = "5";
+        bool thenDoCalled = false;
+        bool thenCalled = false;
+
+        // Act
+        ErrorOr<string> result = await errorOrString
+            .ThenAsync(Convert.ToIntAsync)
+            .Then(num => num + 1)
+            .ThenEnsureAsync(num => Task.FromResult<ErrorOr<int>>(num > 3 ? Error.Validation(description: $"{num} is too big") : num))
+            .ThenDoAsync(_ =>
+            {
+                thenDoCalled = true;
+                return Task.CompletedTask;
+            })
+            .Then(num =>
+            {
+                thenCalled = true;
+                return num.ToString();
+            });
+
+        // Assert
+        thenDoCalled.Should().BeFalse();
+        thenCalled.Should().BeFalse();
+        result.IsError.Should().BeTrue();
+        result.FirstError.Type.Should().Be(ErrorType.Validation);
+        result.FirstError.Description.Should().Be("6 is too big");
+    }
+
+    [Fact]
     public async Task CallingThenEnsureAsyncExtensionMethod_WhenEnsureSucceeds_ShouldReturnOriginalValue()
     {
         // Arrange
--- tests/ErrorOr/ErrorOr.ThenEnsureTests.cs
+++ tests/ErrorOr/ErrorOr.ThenEnsureTests.cs
@@ -58,6 +58,53 @@ public class ThenEnsureTests
     }
 
     [Fact]
+    public void CallingThenEnsureAfterThen_WhenEnsureSucceeds_ShouldContinueChainWithOriginalValue()
+    {
+        // Arrange
+        ErrorOr<string> errorOrString = "5";
+        int valueFromThenDo = 0;
+
+        // Act
+        ErrorOr<string> result = errorOrString
+            .Then(Convert.ToInt)
+            .ThenEnsure(num => num > 3 ? num + 10 : Error.Validation())
+            .ThenDo(num => valueFromThenDo = num)
+            .Then(Convert.ToString);
+
+        // Assert
+        result.IsError.Should().BeFalse();
+        result.Value.Should().Be("5");
+        valueFromThenDo.Should().Be(5);
+    }
+
+    [Fact]
+    public void CallingThenEnsureAfterThen_WhenEnsureReturnsError_ShouldNotInvokeLaterChain()
+    {
+        // Arrange
+        ErrorOr<int> errorOrInt = 5;
+        bool thenDoCalled = false;
+        bool thenCalled = false;
+
+        // Act
+        ErrorOr<int> result = errorOrInt
+            .Then(num => num + 1)
+            .ThenEnsure(num => num > 3 ? Error.Validation(description: $"{num} is too big") : num)
+            .ThenDo(_ => thenDoCalled = true)
+            .Then(num =>
+            {
+                thenCalled = true;
+                return num * 2;
+            });
+
+        // Assert
+        thenDoCalled.Should().BeFalse();
+        thenCalled.Should().BeFalse();
+        result.IsError.Should().BeTrue();
+        result.FirstError.Type.Should().Be(ErrorType.Validation);
+        result.FirstError.Description.Should().Be("6 is too big");
+    }
+
+    [Fact]
     public async Task CallingThenEnsureExtensionMethod_WhenEnsureSucceeds_ShouldReturnOriginalValue()
     {
         // Arrange
